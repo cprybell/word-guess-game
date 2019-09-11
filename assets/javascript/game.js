a5ca1a18e7dbd6e9c0c5ffe8820988d5f7cc96a4
cprybell <cprybell@sbcglobal.net>
2019-09-10 20:00:45 -0700

    have full functionality of the game now. Randomly selects words and listens for the user's guess. If the users guess, if the users guess is contained in the word it replaces the _ with the actual letters. Game resets if the number of guesses left hits zero or if the user has guessed the word.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,6 +5,11 @@ var lettersGuessed = [];
 var guessesLeft = 10;
 var wins = 0;
 var gameWord = document.getElementById("wordGuess");
+var winHTML = document.getElementById("wins");
+var guessesHTML = document.getElementById("guessesLeft");
+var letterGuessedHTML = document.getElementById("lettersGuessed");
+var winCounter = 0;
+var compareLength = 0;
 
 var randomWord = function(arrayIn) {
     var randomNumber = Math.floor(Math.random() * arrayIn.length);
@@ -20,24 +25,90 @@ var createEmptyString = function(word) {
 }
 
 var initializeGame = function() {
-    wordtoGuess = randomWord(words);
-    console.log(wordtoGuess);
-    wordToGuessString = createEmptyString(wordtoGuess);
-    console.log(wordToGuessString);
+    wordToGuess = randomWord(words);
+    //compareLength = wordToGuess.
+    console.log(wordToGuess);
+    wordToGuessString = createEmptyString(wordToGuess);
     gameWord.textContent = wordToGuessString;
+    winHTML.textContent = "You have won: " + wins;
+    guessesHTML.textContent = "Guesses Left: " + guessesLeft;
+    letterGuessedHTML.textContent = "Letters Guessed: " + lettersGuessed;
 }
+
+var resetGame = function() {
+    winCounter = 0;
+    guessesLeft = 10;
+    lettersGuessed = [];
+    wordToGuess = randomWord(words);
+    console.log(wordToGuess);
+    wordToGuessString = createEmptyString(wordToGuess);
+    gameWord.textContent = wordToGuessString;
+    winHTML.textContent = "You have won: " + wins;
+    guessesHTML.textContent = "Guesses Left: " + guessesLeft;
+    letterGuessedHTML.textContent = "Letters Guessed: " + lettersGuessed;
+}
+
+var checkWord = function(word, guessedLetter) {
+    var letterContained = [];
+    for (i = 0; i < word.length; i++) {
+        if (word.charAt(i) == guessedLetter) {
+            letterContained.push(i);
+        }
+    }
+    return letterContained;
+}
+
+var fillWord = function(word, letterArray, letter) {
+    var stringToReturn = "";
+    var arrayCounter = 0;
+    var currentLetter = word.charAt(0);
+    if (letterArray.length == 0) {
+        return word;
+    }
+    else {
+        for (i = 0; i < word.length / 2; i++) {
+            currentLetter = word.charAt(i * 2);
+            if (letterArray[arrayCounter] == i) {
+                stringToReturn = stringToReturn + letter + " ";
+                arrayCounter++;
+            }
+            else {
+                stringToReturn = stringToReturn + currentLetter + " ";
+            }
+        }
+        return stringToReturn;
+    }
+}
+
+console.log(wordToGuess.length);
 
 initializeGame();
 
 document.onkeyup = function(event) {
     var userGuess = event.key;
+    var letterArray = [];
     
     if (!lettersGuessed.includes(userGuess)) {
         lettersGuessed.push(userGuess);
-        console.log(userGuess);
         guessesLeft--;
-        console.log(guessesLeft);
-    } 
-    
+        guessesHTML.textContent = "Guesses Left: " + guessesLeft;
+        letterGuessedHTML.textContent = "Letters Guessed: " + lettersGuessed;
+        letterArray = checkWord(wordToGuess, userGuess);
+        wordToGuessString = fillWord(wordToGuessString, letterArray, userGuess);
+        gameWord.textContent = wordToGuessString;
+        winCounter += letterArray.length;
+    }
 
+    if (winCounter == wordToGuess.length) {
+        wins++;
+        resetGame();
+    }
+    
+    else if (guessesLeft == 0) {
+        resetGame();
+    }
+
+    console.log(wordToGuess);
+    console.log(winCounter);
+    console.log(wordToGuess.length);
 }
