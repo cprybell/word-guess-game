fc9dffc86c9b4d783722a66f3539621fa81f6382
cprybell <cprybell@sbcglobal.net>
2019-09-10 18:40:59 -0700

    added on key listening to the program. Program will also check if the user has guessed the letter or not yet. If user has guessed the letter the program will not take the guess and will not decrement the guesses. Also added a intialize game function that initializes the game.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,43 @@
 var words = ["blue", "yellow", "orange"];
 var wordToGuess = "";
-var lettersGuessed = [""];
+var wordToGuessString = [];
+var lettersGuessed = [];
+var guessesLeft = 10;
+var wins = 0;
+var gameWord = document.getElementById("wordGuess");
 
 var randomWord = function(arrayIn) {
     var randomNumber = Math.floor(Math.random() * arrayIn.length);
     return arrayIn[randomNumber];
 }
 
-wordtoGuess = randomWord(words);
-console.log(wordtoGuess);
+var createEmptyString = function(word) {
+    var stringToReturn = "";
+    for(i = 0; i < word.length; i++) {
+        stringToReturn += "_ ";
+    }
+    return stringToReturn;
+}
+
+var initializeGame = function() {
+    wordtoGuess = randomWord(words);
+    console.log(wordtoGuess);
+    wordToGuessString = createEmptyString(wordtoGuess);
+    console.log(wordToGuessString);
+    gameWord.textContent = wordToGuessString;
+}
+
+initializeGame();
+
+document.onkeyup = function(event) {
+    var userGuess = event.key;
+    
+    if (!lettersGuessed.includes(userGuess)) {
+        lettersGuessed.push(userGuess);
+        console.log(userGuess);
+        guessesLeft--;
+        console.log(guessesLeft);
+    } 
+    
+
+}
